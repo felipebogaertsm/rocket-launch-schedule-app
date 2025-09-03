3a16311dff8d768c4f3cbaee0213e860fdb6c120
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-09-03 11:44:06 -0300

    Added themes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from 'react-native';
+import { useTheme } from '@/styles/theme';
+import { Link } from 'expo-router';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function Index() {
+export default function Home() {
+  const theme = useTheme();
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
+      style={[styles.container, { backgroundColor: theme.colors.background }]}
     >
-      <Text>Hello world.</Text>
+      <Text style={[theme.typography.h1, { color: theme.colors.text }]}>
+        Welcome!
+      </Text>
+      <Text style={[theme.typography.h2, { color: theme.colors.subtitle }]}>
+        Explore the latest rocket launches
+      </Text>
+      <Link href="/launches" asChild>
+        <Button title="Go to Launches" color={theme.colors.primary} />
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+});
